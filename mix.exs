4a1ac7099335c13c38ddfb123178c6908bae54c5
Wojtek Mach <wojtek@wojtekmach.pl>
2018-12-28 22:41:10 +0100

    Add spdx.ex.exs and supporting files

--- mix.exs
+++ mix.exs
@@ -7,11 +7,24 @@ defmodule SPDX.MixProject do
       version: "0.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
   defp deps() do
-    []
+    [
+      {:jason, "~> 1.0", only: [:dev, :test]}
+    ]
+  end
+
+  defp aliases() do
+    [
+      test: [
+        "run lib/spdx.ex.exs",
+        "compile",
+        "test"
+      ]
+    ]
   end
 end
--- test/spdx_test.exs
+++ test/spdx_test.exs
@@ -1,3 +1,15 @@
 defmodule SPDXTest do
   use ExUnit.Case, async: true
+
+  test "version/0" do
+    assert Version.match?(SPDX.semver(), ">= 3.4.0")
+  end
+
+  test "validate_identifier/1" do
+    assert SPDX.validate_identifier("Apache-2.0") == :valid
+
+    assert SPDX.validate_identifier("AGPL-1.0") == :deprecated
+
+    assert SPDX.validate_identifier("bad") == :invalid
+  end
 end
